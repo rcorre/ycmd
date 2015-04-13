1886e646d3c94edd5a4c94c5a361e3f578808a6f
Ryan <ryan@rcorre.net>
2015-04-13 14:33:57 -0400

    Commands to start, stop, and query server

--- ycmd/completers/d/dcd_completer.py
+++ ycmd/completers/d/dcd_completer.py
@@ -48,9 +48,9 @@ class DCDCompleter( Completer ):
 
   def __init__( self, user_options ):
     super( DCDCompleter, self ).__init__( user_options )
+    self._logger = logging.getLogger( __name__ )
     self._dcd_port = None
     self._dcd_phandle = None
-
   # subcommand handling -----------------------------------------
   # pattern of handling subcommands borrowed from CsharpCompleter
 
@@ -60,7 +60,7 @@ class DCDCompleter( Completer ):
     'StopServer': ( lambda self, request_data: self._StopServer() ),
     'RestartServer': ( lambda self, request_data: self._RestartServer(
         request_data ) ),
-    'ServerRunning': ( lambda self, request_data: self.ServerIsRunning() ),
+    'ServerRunning': ( lambda self, request_data: self._ServerRunning() ),
   }
 
   def DefinedSubcommands( self ):
@@ -100,7 +100,7 @@ class DCDCompleter( Completer ):
   # subcommand definitions --------------------------------------
   def _StartServer( self, request_data ):
     """ Start the DCD server """
-    self._logger.info( 'startup' )
+    self._logger.info( 'starting DCD server' )
 
     #Note: detection could throw an exception if an extra_conf_store needs to be confirmed
     #TODO: find dub.json. use dutyl?
@@ -136,12 +136,13 @@ class DCDCompleter( Completer ):
         self._dcd_phandle = utils.SafePopen(
             command, stdout = fstdout, stderr = fstderr, shell = True )
 
-    self._solution_path = path_to_dubfile
+    self._dub_path = path_to_dubfile
 
-    self._logger.info( 'Starting OmniSharp server' )
+    self._logger.info( 'DCD server started on port %d' % self._dcd_port)
 
   def _StopServer( self ):
     """ Stop the DCD server """
+    self._logger.info( 'stopping DCD server' )
     self._ClientCommand("--shutdown")
     self._dcd_phandle.wait()
     self._dcd_port = None
@@ -149,21 +150,19 @@ class DCDCompleter( Completer ):
     if ( not self.user_options[ 'server_keep_logfiles' ] ):
       os.unlink( self._filename_stdout );
       os.unlink( self._filename_stderr );
-    self._logger.info( 'Stopping DCD server' )
+    self._logger.info( 'DCD server stopped' )
 
   def _RestartServer ( self, request_data ):
+    self._logger.info( 'Restarting DCD server' )
     """ Restarts the DCD server """
-    if self.ServerIsRunning():
+    if self._IsServerRunning():
       self._StopServer()
     return self._StartServer( request_data )
 
-  def ServerIsRunning( self ):
-    """ Check if our DCD server is running (up and serving)."""
-    try:
-      return bool( self._dcd_port and
-                   self._ClientCommand("--query") == 0)
-    except:
-      return False
+  def _ServerRunning ( self ):
+    if self._IsServerRunning():
+      return responses.BuildDisplayMessageResponse("DCD server is UP")
+    return responses.BuildDisplayMessageResponse("DCD server is DOWN")
 
   # end subcommand definitions ----------------------------------
 
@@ -176,7 +175,11 @@ class DCDCompleter( Completer ):
     self._logger.info( u'using port {0}'.format( self._dcd_port ) )
 
   def _ClientCommand( self, args ):
-    command = PATH_TO_DCD_SERVER_BINARY + ' ' + args
+    command = "{binpath} -p{port} {args}".format(
+      binpath = PATH_TO_DCD_CLIENT_BINARY,
+      port = self._dcd_port,
+      args = args)
+    self._logger.info('client command: ' + command)
     with open( self._filename_stderr, 'w' ) as fstderr:
       with open( self._filename_stdout, 'w' ) as fstdout:
         # shell=True is needed for Windows so DCD does not spawn
@@ -184,6 +187,15 @@ class DCDCompleter( Completer ):
         client_proc = utils.SafePopen(
             command, stdout = fstdout, stderr = fstderr, shell = True )
         return client_proc.wait()
+    #self._logger.info('client command complete')
+
+  def _IsServerRunning( self ):
+    """ Check if our DCD server is running (up and serving)."""
+    try:
+      return (self._dcd_port is not None)# and
+             #(self._ClientCommand("--query") == 0)
+    except:
+      return False
 
   # end helper functions ----------------------------------------
 
